702309df2a8c1b2ef9d303dc81591ba564bff9ae
Tom <35542432+AquaBx@users.noreply.github.com>
2026-08-08 15:49:57 +0200

    changes to webpack + paraglide build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from 'next';
 import { createRequire } from 'node:module';
+import { paraglideWebpackPlugin } from "@inlang/paraglide-js";
 
 const require = createRequire(import.meta.url);
 
 const config: NextConfig = {
+  webpack: (config) => {
+    config.plugins.push(
+      paraglideWebpackPlugin({
+        project: './src/project.inlang',
+        outdir: './src/paraglide',
+        emitTsDeclarations: true,
+      })
+    );
+    return config;
+  },
   adapterPath: require.resolve('adapter-bun'),
 };
 
