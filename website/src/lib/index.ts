24892d0d3e15fdc835d98fce3f918859e18872bc
Tom <35542432+AquaBx@users.noreply.github.com>
2026-09-24 02:46:00 +0200

    change env

--- website/src/lib/index.ts
+++ website/src/lib/index.ts
@@ -5,5 +5,5 @@ import { env } from '$env/dynamic/public';
 export function getMediaUrl(url: string | null | undefined) {
     if (!url) return '';
     if (url.startsWith('http://') || url.startsWith('https://')) return url;
-    return `${env.CMS_URL || 'http://localhost:3001'}/${url}`;
+    return `${env.PUBLIC_CMS_URL || 'http://localhost:3001'}/${url}`;
 }
